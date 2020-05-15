b627e5b73319fc38d4f5847df83a3a1abe74be36
navi1995 <navi1995@hotmail.com>
2020-05-15 14:42:28 +1200

    Refactor and added specific getting of movies without adding to list, and deleting from list.

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 /*
 	TODOS/Approach
 	1. Running polls, grab random 10 movies from list and gather reacts, if repoll react hit with admin then re-roll.fail
-	2. Delete specific movie
+	2. Delete specific movie **
 	3. Confirmation if search results > 1?
-	4. Allow IMDB links to be submitted
+	4. Allow IMDB links to be submitted  **
 	5. Move commands into self files
 	6. Roulette mode
 	7. Auto-delete mode
-	8. Test duplicates
+	8. Test duplicates **
+	9. Get details for specific movie, either in DB or general search. **
 */
 const fs = require("fs");
 const Discord = require("discord.js");
@@ -22,6 +23,7 @@ const Schema = mongoose.Schema;
 const ObjectId = Schema.ObjectId;
 const Movie = new Schema({
 	id: ObjectId,
+	primaryKey: {type: String, unique: true },
 	guildID: {type: String, index: true },
 	movieID: String,
 	imdbID: String,
@@ -60,9 +62,11 @@ client.on("message", async message => {
 		const movieEmbed = new Discord.MessageEmbed().setTitle("Submitted Movies");
 		var number = 1;
 		var description = "";
+		var searchOptions = searchMovieDatabaseObject(message.guild.id, "");
 
 		//25 embed limit for fields
-		movieModel.find({}, function (error, docs) {
+		return movieModel.find(searchOptions, function (error, docs) {
+			console.log(docs);
 			if (docs && docs.length > 0) {
 				docs.forEach(function(movie) {
 					description += `**[${number}. ${movie.name}](https://www.imdb.com/title/${movie.imdbID})** submitted by ${movie.submittedBy} on ${moment(movie.submitted).format("DD MMM YYYY")}\n
@@ -76,61 +80,74 @@ client.on("message", async message => {
 		});
 	}
 
-	if (message.guild && message.content == "deleteall") {
-		movieModel.deleteMany({guildID: message.guild.id}, handleError);
-	}
+	if (message.guild && message.content.startsWith("getspecific")) {
+		//10 limit embeds per image
+		var movie = message.content.replace("getspecific", "").trimStart();
+		var searchOptions = searchMovieDatabaseObject(message.guild.id, movie);
+		var movieEmbed;
 
-	if (message.guild && message.content.startsWith("testsearch")) {
-		var failedSearch = false;
-		var data = false;
-		var initialData = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${movieDbAPI}&query=${encodeURIComponent(message.content.replace("testsearch", "").trimStart())}&page=1`).then(response => response.json());
-
-		if (!initialData || initialData.total_results == 0) {
-			failedSearch = true;
-		}
-
-		if (!failedSearch) {
-			data = await fetch(`https://api.themoviedb.org/3/movie/${initialData.results[0].id}?api_key=${movieDbAPI}`).then(response => response.json());
-		}
-
-		if (!data || failedSearch) {
-			message.channel.send("Couldn't find any movies. Sorry!");
-			return;
-		} 
-
-		const newMovie = new movieModel({
-			guildID: message.guild.id,
-			movieID: data.id,
-			imdbID: data.imdb_id,
-			name: data.original_title,
-			posterURL: `https://image.tmdb.org/t/p/original/${data.poster_path}`,
-			overview: data.overview,
-			releaseDate: new Date(data.release_date),
-			runtime: data.runtime,
-			rating: data.vote_average,
-			submittedBy: message.member.user
-		});
-		
-		newMovie.save(function(err) {
-			handleError(err, "Couldn't add movie.");
-
-			if (!err) {
-				const movieEmbed = new Discord.MessageEmbed()
-					.setAuthor("Movie Added!")
-					.setTitle(newMovie.name)
-					.setURL(`https://www.imdb.com/title/${newMovie.imdb_id}`)
-					.setDescription(newMovie.overview)
-					.setThumbnail(newMovie.posterURL)
-					.addFields(
-						{ name: "Release Date", value: moment(newMovie.releaseDate).format("DD MMM YYYY"), inline: true },
-						{ name: "Runtime", value: newMovie.runtime + " Minutes", inline: true },
-						{ name: "Rating", value: newMovie.rating, inline: true },
-						{ name: "Submitted By", value: newMovie.submittedBy, inline: true }
-					)
-
-				message.channel.send(movieEmbed);
+		//25 embed limit for fields
+		return movieModel.findOne(searchOptions, function (error, movie) {
+			if (movie) {
+				movieEmbed = buildSingleMovieEmbed(movie);
+				message.channel.send(movieEmbed);		
 			}
 		});
+	}
+
+	if (message.guild && message.content == "deleteall") {
+		movieModel.deleteMany({guildID: message.guild.id}, function(err) {
+			if (!err) {
+				return message.channel.send("All movies have been deleted.");
+			}
+		});
+	}
+
+	if (message.guild &&  message.content.startsWith("deletespecific")) {
+		var movie = message.content.replace("deletespecific", "").trimStart();
+		var searchOptions = searchMovieDatabaseObject(message.guild.id, movie);
+		
+		if (movie != "") {
+			return movieModel.findOneAndDelete(searchOptions, function(err, movie) {
+				if (err || !movie) {
+					message.channel.send("Movie could not be found!");
+				} else {
+					message.channel.send(`Movie deleted: ${movie.name}`);
+				}
+			});
+		} else {
+			return message.channel.send("No empty args");
+		}
+	}
+
+	if (message.guild && message.content.startsWith("addmovie")) {
+		var search = message.content.replace("addmovie", "").trimStart();
+		var newMovie = await searchNewMovie(search, message);
+		
+		if (newMovie) {
+			newMovie.save(function(err) {
+				if (err && err.name == "MongoError") {
+					message.channel.send("Movie already exists in the list.");
+				}
+
+				if (!err) {
+					const movieEmbed = buildSingleMovieEmbed(newMovie, "Movie Added!");
+
+					message.channel.send(movieEmbed);
+				}
+			});
+		}		
+	}
+
+	if (message.guild && message.content.startsWith("search")) {
+		var search = message.content.replace("search", "").trimStart();
+		var newMovie = await searchNewMovie(search, message);
+		
+		if (newMovie) {
+			const movieEmbed = buildSingleMovieEmbed(newMovie, "Movie Details (Not Added)");
+			
+			message.channel.send(movieEmbed);
+		}		
 	}
 
 	if (!message.content.startsWith(prefix) || message.author.bot) return;
@@ -189,3 +206,83 @@ function handleError(err, message) {
 		console.log(message);
 	}
 }
+
+//Movie can be string or IMDB link
+function searchMovieDatabaseObject(guildID, movie) {
+	var searchObj = {
+		guildID: guildID
+	}
+	
+	if (movie != "" && movie) {
+		searchObj.name = new RegExp(".*" + movie + ".*", "i");
+	}
+
+	return searchObj;
+}
+
+function buildSingleMovieEmbed(movie, subtitle) {
+	var embed = new Discord.MessageEmbed()
+		.setTitle(movie.name)
+		.setURL(`https://www.imdb.com/title/${movie.imdb_id}`)
+		.setDescription(movie.overview)
+		.setThumbnail(movie.posterURL)
+		.addFields(
+			{ name: "Release Date", value: moment(movie.releaseDate).format("DD MMM YYYY"), inline: true },
+			{ name: "Runtime", value: movie.runtime + " Minutes", inline: true },
+			{ name: "Rating", value: movie.rating, inline: true },
+			{ name: "Submitted By", value: movie.submittedBy, inline: true }
+		);
+
+	if (subtitle) {
+		embed.setAuthor(subtitle)
+	}
+
+	return embed;
+}
+
+async function searchNewMovie(search, message) {
+	var failedSearch = false;
+	var data = false;
+	var isImdbSearch = search.indexOf("imdb.com") > 0;
+	var searchTerm = isImdbSearch ? (search.match(/tt[0-9]{7,8}/g) != null ? search.match(/tt[0-9]{7,8}/g) : null) : search;
+
+	if (searchTerm == "" || !searchTerm) {
+		message.channel.send("Please enter a valid search."); 
+		return;
+	}
+
+	var initialData = !isImdbSearch ? await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${movieDbAPI}&query=${encodeURIComponent(searchTerm)}&page=1`).then(response => response.json()) : await fetch(`https://api.themoviedb.org/3/find/${encodeURIComponent(searchTerm)}?api_key=${movieDbAPI}&external_source=imdb_id`).then(response => response.json());
+
+	if (!initialData || initialData.total_results == 0 || (initialData.movie_results && initialData.movie_results.length == 0)) {
+		failedSearch = true;
+	}
+
+	if (!failedSearch) {
+		data = await fetch(`https://api.themoviedb.org/3/movie/${isImdbSearch ? initialData.movie_results[0].id : initialData.results[0].id}?api_key=${movieDbAPI}`).then(response => response.json());
+	}
+
+	if (!data || failedSearch) {
+		message.channel.send("Couldn't find any movies. Sorry!");
+		return;
+	} 
+
+	return new movieModel({
+		primaryKey: message.guild.id + data.id,
+		guildID: message.guild.id,
+		movieID: data.id,
+		imdbID: data.imdb_id,
+		name: data.original_title,
+		posterURL: `https://image.tmdb.org/t/p/original/${data.poster_path}`,
+		overview: data.overview,
+		releaseDate: new Date(data.release_date),
+		runtime: data.runtime,
+		rating: data.vote_average,
+		submittedBy: message.member.user
+	});
+}
+
+
+	// if (movie.indexOf("imdb.com") > 0) {
+	// 	movie = movie.substr(movie.indexOf("/tt")).replace(/\//ig, "");
+	// 	searchObj.imdbID = movie;
+	// } else 
