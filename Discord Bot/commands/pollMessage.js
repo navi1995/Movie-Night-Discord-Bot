7a6698209304131f4070172dac261f0a7e34c57e
Alexander Chang-Davidson <changdavidson4@gmail.com>
2021-03-14 12:48:53 -0700

    clean up copy pasted code, make sure to await message sends to prevent lost promises

--- Discord Bot/commands/pollMessage.js
+++ Discord Bot/commands/pollMessage.js
@@ -1,21 +1,21 @@
 module.exports = {
 	name: "pollmessage",
 	description: "Updates pollmessage to chosen string.",
-	usage: "[prefix chosen]",
+	usage: "[poll message]",
 	admin: true,
 	async execute(message, args, main, callback, settings) {
 		if (!args.length) {
-			message.channel.send(`Poll message is currently set to: ${settings.pollMessage}`);
+			await message.channel.send(`Poll message is currently set to: ${settings.pollMessage}`);
 
 			return callback();
 		} else {
-			var pollMessage = args.join(" ").trim();
+			const pollMessage = args.join(" ").trim();
 
-			return main.setting.updateOne({guildID: message.guild.id}, { "pollMessage": pollMessage }, function(err) {
+			return main.setting.updateOne({ guildID: message.guild.id }, { pollMessage }, err => {
 				if (!err) {
-					message.channel.send(`Poll message has now been set to: ${pollMessage}`);
+					await message.channel.send(`Poll message has now been set to: ${pollMessage}`);
 				} else {
-					message.channel.send("Couldn't set Poll message, something went wrong");
+					await message.channel.send("Couldn't set Poll message, something went wrong");
 				}
 
 				return callback();
