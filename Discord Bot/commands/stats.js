bcd913a64a69e6bc7fe340865bd5d26a41d838b2
Alexander Chang-Davidson <changdavidson4@gmail.com>
2021-03-14 14:59:15 -0700

    embed stats

--- Discord Bot/commands/stats.js
+++ Discord Bot/commands/stats.js
@@ -4,14 +4,18 @@ module.exports = {
 	async execute(message, args, main) {
 		const promises = [
 			main.client.shard.fetchClientValues('guilds.cache.size'),
-			main.client.shard.broadcastEval('this.guilds.cache.reduce((acc, guild) => acc + guild.memberCount, 0)'),
+			main.client.shard.broadcastEval('this.guilds.cache.reduce((acc, guild) => acc + guild.memberCount)'),
 		];
 		
-		Promise.all(promises)
+		return Promise.all(promises)
 			.then(results => {
 				const totalGuilds = results[0].reduce((acc, guildCount) => acc + guildCount, 0);
 				const totalMembers = results[1].reduce((acc, memberCount) => acc + memberCount, 0);
-				return message.channel.send(`Server count: ${totalGuilds}\nMember count: ${totalMembers}`);
+				return message.channel.send({ embed: {
+					color: 0x03a9f4,
+					title: 'Bot Stats',
+					description: `Server count: ${totalGuilds}\nMember count: ${totalMembers}`
+				} });
 			}).catch(console.error);
 	}
 };
