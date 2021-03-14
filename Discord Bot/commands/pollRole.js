17af62761bbd6a324cec820ce5c2fb77a1622196
Alexander Chang-Davidson <changdavidson4@gmail.com>
2021-03-14 13:06:20 -0700

    allow pollRole to not be mentioned

--- Discord Bot/commands/pollRole.js
+++ Discord Bot/commands/pollRole.js
@@ -1,27 +1,26 @@
 module.exports = {
 	name: "pollrole",
-	description: "Sets a role that is allowed to run polls. Can also clear this role by using pollrole remove",
-	usage: "[@roleName]",
+	description: "Sets a role that is allowed to run polls. Can also clear this role by using pollrole clear",
+	usage: "[roleName]",
 	args: true,
 	admin: true,
 	async execute(message, args, main, callback) {
-		if (args.length > 1) {
-			message.channel.send("Please only specify one mentioned role.");
+		let pollRole = args.join(' ');
+		pollRole = (pollRole.match(/<@&([0-9]{17,21})>/) || [])[1] || (message.guild.roles.cache.find(r => pollRole !== "clear" && r.name === pollRole) || {}).id || pollRole;
 
-			return callback();
-		} else if (!message.mentions.roles.first() && args[0] != "clear") {
-			message.channel.send("Please mention the role you'd like to set in the format pollrole [@roleName], or to clear settings use pollrole clear");
+		if (!message.guild.roles.resolve(pollRole) && pollRole !== "clear") {
+			await message.channel.send("Please provide a valid role you'd like to set in the format `pollrole [roleName]`, or to clear settings use `pollrole clear`");
 
 			return callback();
 		} else {
-			const pollRole = args[0] == "clear" ? null : message.mentions.roles.first().id;
+			pollRole = pollRole === "clear" ? null : pollRole;
 
 			//Update the settings with the role user provided, or clear it and set to NULL.
-			return main.setting.updateOne({ guildID: message.guild.id }, { "pollRole": pollRole }, function(err) {
+			return main.setting.updateOne({ guildID: message.guild.id }, { pollRole }, err => {
 				if (!err) {
-					message.channel.send(pollRole ? `Users with administrator or the role ${args[0]} will now be able to run polls.` : "Setting for role allowed to run polls has been cleared. Only admins may run polls now.");
+					await message.channel.send(pollRole ? `Users with administrator or the role <@&${pollRole}> will now be able to run polls.` : "Setting for role allowed to run polls has been cleared. Only admins may run polls now.");
 				} else {
-					message.channel.send("Couldn't set role for adding permissions, something went wrong");
+					await message.channel.send("Couldn't set role for adding permissions, something went wrong");
 				}
 
 				return callback();
