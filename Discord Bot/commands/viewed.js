165aafba3dd5ff1c42841a67db72b7210447e644
Alexander Chang-Davidson <changdavidson4@gmail.com>
2021-03-14 15:11:31 -0700

    no var, handle errors properly

--- Discord Bot/commands/viewed.js
+++ Discord Bot/commands/viewed.js
@@ -6,45 +6,42 @@ module.exports = {
 	description: "Returns list of all movies that have been marked as viewed for server.",
 	aliases: ["getviewed", "viewedlist"],
 	execute(message, args, main) {
-		var embeddedMessages = [];
-		var number = 1;
-		var description = "";
-		var searchOptions = main.searchMovieDatabaseObject(message.guild.id, "");
-		var movieEmbed = new MessageEmbed().setTitle("Viewed Movies").setColor("#6441a3");
+		let embeddedMessages = [];
+		let number = 1;
+		let description = "";
+		let searchOptions = main.searchMovieDatabaseObject(message.guild.id, "");
+		let movieEmbed = new MessageEmbed().setTitle("Viewed Movies").setColor("#6441a3");
 
 		searchOptions.viewed = true;
 
 		//2048 limit
-		return main.movieModel.find(searchOptions, function (error, docs) {
-			if (docs.length == 0) {
-				message.channel.send("List of unviewed movies is currently empty.");
-
-				return;
+		return main.movieModel.find(searchOptions, async function (error, docs) {
+			if (error) return message.channel.send("Something went wrong trying to find the movies");
+			
+			if (!docs || !docs.length) {
+				return message.channel.send("List of unviewed movies is currently empty.");
 			}
 
-			if (docs && docs.length > 0) {
-				for (var movie of docs) {
-					var stringConcat = `**[${number}. ${movie.name}](https://www.imdb.com/title/${movie.imdbID})** submitted by <@${movie.submittedBy}>, viewed on ${moment(movie.viewedDate).format("DD MMM YYYY")}\n
-					**Release Date:** ${moment(movie.releaseDate).format("DD MMM YYYY")} **Runtime:** ${movie.runtime} **Minutes Rating:** ${movie.rating}\n\n`;
+			for (let movie of docs) {
+				const stringConcat = `**[${number++}. ${movie.name}](https://www.imdb.com/title/${movie.imdbID})** submitted by <@${movie.submittedBy}>, viewed on ${moment(movie.viewedDate).format("DD MMM YYYY")}\n
+				**Release Date:** ${moment(movie.releaseDate).format("DD MMM YYYY")} **Runtime:** ${movie.runtime} **Minutes Rating:** ${movie.rating}\n\n`;
 
-					//If the length of message has become longer than DISCORD API max, we split the message into a seperate embedded message.
-					if (description.length + stringConcat.length > 2048) {
-						movieEmbed.setDescription(description);
-						embeddedMessages.push(movieEmbed);
-						description = "";
-						movieEmbed = new MessageEmbed().setTitle("Viewed Movies (Cont...)").setColor("#6441a3");
-					} 
+				//If the length of message has become longer than DISCORD API max, we split the message into a seperate embedded message.
+				if (description.length + stringConcat.length > 2048) {
+					movieEmbed.setDescription(description);
+					embeddedMessages.push(movieEmbed);
+					description = "";
+					movieEmbed = new MessageEmbed().setTitle("Viewed Movies (Cont...)").setColor("#6441a3");
+				} 
 
-					description += stringConcat;
-					number++;
-				}
+				description += stringConcat;
 			}
 
 			movieEmbed.setDescription(description);
 			embeddedMessages.push(movieEmbed);
 
-			for (var embeddedMessage of embeddedMessages) {
-				message.channel.send(embeddedMessage);
+			for (let embeddedMessage of embeddedMessages) {
+				await message.channel.send(embeddedMessage);
 			}
 
 			return;
