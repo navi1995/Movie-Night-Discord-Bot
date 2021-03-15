e4c4d7bd09cb1b0123ee296568608e7ba8f7e285
Navi Jador <navi1995@hotmail.com>
2021-03-15 20:51:42 +1300

    Fix stats command

--- Discord Bot/commands/stats.js
+++ Discord Bot/commands/stats.js
@@ -4,18 +4,28 @@ module.exports = {
 	async execute(message, args, main) {
 		const promises = [
 			main.client.shard.fetchClientValues('guilds.cache.size'),
-			main.client.shard.broadcastEval('this.guilds.cache.reduce((acc, guild) => acc + guild.memberCount)', 0),
+			main.client.shard.broadcastEval('this.guilds.cache.reduce((acc, guild) => acc + guild.memberCount, 0)'),
 		];
 		
 		return Promise.all(promises)
 			.then(results => {
 				const totalGuilds = results[0].reduce((acc, guildCount) => acc + guildCount, 0);
 				const totalMembers = results[1].reduce((acc, memberCount) => acc + memberCount, 0);
-				return message.channel.send({ embed: {
+
+				return message.channel.send({embed: {
 					color: 0x03a9f4,
 					title: 'Bot Stats',
-					description: `Server count: ${totalGuilds}\nMember count: ${totalMembers}`
-				} });
+					fields: [
+						{
+							name: "Server Count",
+							value: totalGuilds
+						},
+						{
+							name: "Member Count",
+							value: totalMembers
+						}
+					]
+				}});
 			}).catch(console.error);
 	}
 };
