6ba48e85060f1dfce6421bcb95c0e43c4cbf0887
Navi Jador <navi1995@hotmail.com>
2023-05-19 07:54:29 +1000

    Implement hybrid sharding.

--- Discord Bot/index.js
+++ Discord Bot/index.js
@@ -1,11 +1,23 @@
 const { token } = require("./config.json");
-const { ShardingManager } = require('discord.js');
-const manager = new ShardingManager('./bot.js', { 
-	token,
-	mode: "worker",
-	respawn: true
- });
+const { ClusterManager } = require("discord-hybrid-sharding");
+// const { ShardingManager } = require("discord.js");
+// const manager = new ShardingManager('./bot.js', {
+// 	token,
+// 	mode: "worker",
+// 	respawn: true,
+// 	totalShards: 2
+//  });
+const manager = new ClusterManager("./bot.js", {
+	totalShards: "auto", // or 'auto'
+	/// Check below for more options
+	shardsPerClusters: 5,
+	totalClusters: 2,
+	mode: "worker", // you can also choose "worker"
+	token: token
+});
 
-manager.on('shardCreate', shard => console.log(`Launched shard ${shard.id}`))
+manager.on("clusterCreate", (cluster) => console.log(`Launched Cluster ${cluster.id}`));
+manager.spawn({ timeout: -1 });
 
-manager.spawn({timeout: -1});
+// manager.on("shardCreate", (shard) => console.log(`Launched shard ${shard.id}`));
+// manager.spawn({ timeout: -1 });
